6e14243145689818453a98abdf29dd775b02feb3
damasodaniel <danieltelles8a@gmail.com>
2021-09-29 22:30:01 -0500

    Creación de variables y constantes.

--- source/ConsoleAppHolaMundo/ConsoleAppHolaMundo/Module1.vb
+++ source/ConsoleAppHolaMundo/ConsoleAppHolaMundo/Module1.vb
@@ -2,11 +2,12 @@
 
     Sub Main()
 
-        Console.WriteLine("Hola a todos")
+        Dim numero1 As Integer = 35
+        Const PULGADA As Double = 2.54
+
+        Console.WriteLine(numero1)
 
         Console.WriteLine("Bienvenidos al curso de VB.Net")
-
-
 
 
     End Sub
